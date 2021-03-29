ec4d3833062b5479dd0e9a339219a0acab5dec71
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-29 17:06:03 +0100

    feat: add layout and headers for home and non-home pages. Placeholder content

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Reservations Index")
+def home(request):
+    return render(request, "reservations/home.html")
+
+def booking(request):
+    return render(request, "reservations/booking.html")

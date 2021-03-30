b159ca5412587bcc8bddb8b495b86cc5f69a3f0c
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-30 07:24:14 +0100

    feat: finalize homepage content

--- reservations/util.py
+++ reservations/util.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from django.db.models import Max
 from django.db.models.deletion import SET_NULL, PROTECT
+import random
 
 def NULL_OR_PROTECT(collector, field, sub_objs, using):
     for booking in sub_objs:
@@ -7,3 +9,15 @@ def NULL_OR_PROTECT(collector, field, sub_objs, using):
             PROTECT(collector, field, sub_objs, using)
             return
     SET_NULL(collector, field, sub_objs, using)
+
+def get3RandomRoomIds(Room):
+    max_id = Room.objects.all().aggregate(max_id=Max("id"))['max_id']
+    room_ids = []
+    while len(room_ids) < 3:
+        key = random.randint(1, max_id)
+        if key not in room_ids:
+            room = Room.objects.filter(pk=key).first()
+            if room:
+                room_ids.append(key)
+
+    return room_ids
--- reservations/views.py
+++ reservations/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from reservations.models import *
 from django.db.models import Max
+from reservations.util import get3RandomRoomIds
 
 def home(request):
+    room_ids = get3RandomRoomIds(Room)
+
+    room1 = Room.objects.filter(pk=room_ids[0]).get()
+    room2 = Room.objects.filter(pk=room_ids[1]).get()
+    room3 = Room.objects.filter(pk=room_ids[2]).get()
+
+    room1Img = room1.pictured_room.first()
+    room2Img = room2.pictured_room.first()
+    room3Img = room3.pictured_room.first()
+
     return render(request, "reservations/home.html", {
+        "room1Img": room1Img,
+        "room2Img": room2Img,
+        "room3Img": room3Img,
+        "room1": room1,
+        "room2": room2,
+        "room3": room3,
     })
 
 def room(request, id):

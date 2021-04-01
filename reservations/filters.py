9aa3aceba423208713829f608d28663593ee3f9a
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-04-01 10:38:01 +0100

    feat: make filter form auto submit on change

--- reservations/filters.py
+++ reservations/filters.py
@@ -1,9 +1,10 @@
 from reservations.models import *
 import django_filters
+from django_filters.widgets import LinkWidget
 
 class RoomTypeFilter(django_filters.FilterSet):
     class Meta:
         model = Room
         fields = {
             'type': ['exact']
-        }
+        }       
